3f273a16287cb7f5d66879a5edd7d3a42e886ab3
Chris Ross <Tratcher@Outlook.com>
2023-06-07 18:06:44 -0700

    Update the dotnet sync action (#8)

--- .github/workflows/ReportDiff.ps1
+++ .github/workflows/ReportDiff.ps1
@@ -1,6 +1,7 @@
 # Check the code is in sync
 $changed = (select-string "nothing to commit" artifacts\status.txt).count -eq 0
 if (-not $changed) { return $changed }
+
 # Check if tracking issue is open/closed
 $Headers = @{ Authorization = 'token {0}' -f $ENV:GITHUB_TOKEN; };
 $result = Invoke-RestMethod -Uri $issue -Headers $Headers
@@ -26,6 +27,12 @@ $diff
 $json = ConvertTo-Json -InputObject @{ 'body' = $body }
 $issue = $issue + '/comments'
 $result = Invoke-RestMethod -Method POST -Headers $Headers -Uri $issue -Body $json
+
+# Make a branch
+$timestamp = get-date -format "yyyy-MMM-dd-HH-mm-ss"
+$branch = "github-action/sync-dotnet-extensions-" + $timestamp
+cd azure-extensions
+git checkout -b $branch
 
 # Check if there's an open PR in Azure or Dotnet orgs to resolve this difference.
 $sendpr = $true
